ea68b5dbf7ad24139e6192c603e324b08d972af2
Colby Sullivan <sullivanc@southwestern.edu>
2024-05-20 19:08:34 -0500

    Clean up and fixed movement
    Added some TODOs but a basic controller is finished

--- scripts/playercontroller.gd
+++ scripts/playercontroller.gd
@@ -7,19 +7,20 @@ const SPEED = 300.0
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# TODO this could use some optimization as it is simply just a placeholder
+	# also there is a snapback issue with animation currently that can be fixed later
 	var horizontal_direction = Input.get_axis("ui_left", "ui_right")
 	var vertical_direction = Input.get_axis("ui_up", "ui_down")
-	if horizontal_direction:
+	if horizontal_direction || vertical_direction:
 		velocity.x = horizontal_direction * SPEED
-	if vertical_direction:
 		velocity.y = vertical_direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.x = move_toward(velocity.normalized().x, 0, SPEED)
+		velocity.y = move_toward(velocity.normalized().y, 0, SPEED)
 		
-	check_animation_orientation()
+	set_animation()
 	move_and_slide()
 
-func check_animation_orientation():
+# Use this when more states are added into the animation tree if we decide to use it
+func set_animation():
 	animated_tree.set("parameters/walking/blend_position", velocity)
