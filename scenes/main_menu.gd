8589662ddda34f7432edfb0d5999bb5cbfd48494
willmaier <maier.w@northeastern.edu>
2024-05-24 13:33:06 -0400

    Basic main menu scene

---
+++ scenes/main_menu.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+func _on_play_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
