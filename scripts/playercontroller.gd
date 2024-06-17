ef5dd95ec8dfb002861aff33ab3f4efa9bf664af
willmaier <maier.w@northeastern.edu>
2024-06-17 11:28:53 -0400

    Added animations (NOT DONE)
    Redid some of the movement code. Need spritesheet redone but should be seamless to just replace it when it gets resized

--- scripts/playercontroller.gd
+++ scripts/playercontroller.gd
@@ -5,6 +5,7 @@ const speed = 300.0
 # Player tree animation
 @onready var animated_sprite = $AnimationPlayer
 @onready var animated_tree = $AnimationTree
+@onready var state_machine = animated_tree.get("parameters/playback")
 
 # Sound handling
 @onready var player_walk_sound = $PlayerWalkSound
@@ -21,28 +22,46 @@ func _physics_process(_delta):
 	# Get the input direction and handle the movement/deceleration
 	# TODO this could use some optimization as it is simply just a placeholder
 	# also there is a snapback issue with animation currently that can be fixed later
-	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	var vertical_direction = Input.get_axis("move_up", "move_down")
-	if horizontal_direction || vertical_direction:
-		velocity.x = horizontal_direction * speed
-		velocity.y = vertical_direction * speed
-	else:
-		velocity.x = move_toward(velocity.normalized().x, 0, speed)
-		velocity.y = move_toward(velocity.normalized().y, 0, speed)
+	#var horizontal_direction = Input.get_axis("move_left", "move_right")
+	#var vertical_direction = Input.get_axis("move_up", "move_down")
+	#if horizontal_direction || vertical_direction:
+		#velocity.x = horizontal_direction * speed
+		#velocity.y = vertical_direction * speed
+	#else:
+		#velocity.x = move_toward(velocity.normalized().x, 0, speed)
+		#velocity.y = move_toward(velocity.normalized().y, 0, speed)
+		#player_walk_sound.play()
+	
+	var input_direction = Vector2(
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+	set_animation(input_direction)
+	velocity = input_direction * speed
+	
+	if input_direction == Vector2.ZERO:
 		player_walk_sound.play()
 	
 	if (crafting_table.player_present and Input.is_action_just_pressed("interact")):
 		toggle_crafting()
 
-	set_animation()
+	
 	move_and_slide()
+	pick_new_state()
 
 # TODO Use this when more states are added into the animation tree if we decide to use it
-func set_animation():
-	if velocity != Vector2(0,0):
-		animated_tree.set("parameters/walking/blend_position", velocity)
-# Gives player access to the inventory
+func set_animation(move_input : Vector2):
+	if (move_input != Vector2.ZERO):
+		animated_tree.set("parameters/walking/blend_position", move_input)
+		animated_tree.set("parameters/idle/blend_position", move_input)
 
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("walking")
+	else:
+		state_machine.travel("idle")
+
+# Gives player access to the inventory
 func collect(item):
 	inv.insert(item)
 
