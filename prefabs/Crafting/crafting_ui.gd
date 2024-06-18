e9bffb8a18fd48f79190b35a406b0b89bee5be6e
willmaier <maier.w@northeastern.edu>
2024-06-18 09:43:15 -0400

    Fixed UI stuff

--- prefabs/Crafting/crafting_ui.gd
+++ prefabs/Crafting/crafting_ui.gd
@@ -1,5 +1,10 @@
 extends Control
 
+@export var mat1: HBoxContainer
+@export var mat2: HBoxContainer
+@export var tool1: HBoxContainer
+@export var tool2: HBoxContainer
+
 func _process(_delta):
 	if Globals.is_crafting:
 		visible = true
@@ -12,6 +17,7 @@ func _on_tool_1_button_pressed():
 		Globals.rock_count -= 1
 		Globals.hammer_tool = true
 		Globals.harvest_speed = 3
+		tool1.queue_free()
 		print("hammer made")
 
 func _on_tool_2_button_pressed():
@@ -19,4 +25,5 @@ func _on_tool_2_button_pressed():
 		Globals.stick_count -= 10
 		Globals.rock_count -= 10
 		Globals.pickaxe_tool = true
+		tool2.queue_free()
 		print("Pickaxe made")
