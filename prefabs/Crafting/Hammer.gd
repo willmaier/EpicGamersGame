5edb5fee28f571d5a120ee52af15180e4e1849ff
Swijii <158099473+Swijii@users.noreply.github.com>
2024-06-20 20:57:05 +0100

    Delete Branch

--- prefabs/Crafting/Hammer.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-var crafting_ui = get_parent()
-
-# Called when the node enters the scene tree for the first time.
-func _on_pressed():
-	crafting_ui._on_hammer_pressed()
--- prefabs/Inventory/Slot_data/Inv_Slot.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-
-class_name InvSlot
-
-@export var item: Inv_Item
-@export var amount: int 
