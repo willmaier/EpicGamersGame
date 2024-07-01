58ea6924952963f4f623a1e2b85233e10ccc2951
willmaier <maier.w@northeastern.edu>
2024-07-01 11:11:15 -0400

    Added credits and pause menus

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -5,7 +5,7 @@ func _on_play_button_pressed():
 
 
 func _on_options_button_pressed():
-	pass # Replace with function body.
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
 
 
 func _on_quit_button_pressed():
---
+++ scenes/pause.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_resume_pressed():
+	get_tree().paused = false
