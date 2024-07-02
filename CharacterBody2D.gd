4fd8261c137c0c0c0ba7476cd20c62caf65d6274
willmaier <maier.w@northeastern.edu>
2024-07-02 01:33:49 -0400

    That's all folks!

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -3,13 +3,22 @@ extends CharacterBody2D
 var speed = 40
 var animal_flee = false   
 var player = null
+@export var anim_player: AnimationPlayer
 
 func _physics_process(delta):
 	if animal_flee:
 		
 		position-= (player.position - position)/speed
 		
-		
+		if (velocity.x < 0 ):
+			anim_player.play("beaver_left")
+		elif (velocity.x < velocity.y and velocity.y < 0):
+			anim_player.play("beaver_up")
+		elif (velocity.x > 0 and velocity.y < velocity.x):
+			anim_player.play("beaver_right")
+		else:
+			pass
+			#anim_player.play("beaver_down")
 
 
 func _on_area_2d_body_entered(body):
